5f729f5ae33343ae1d6e42fda6c0ea9875b4ed9e
Rounak Meyur <45717264+rounak-meyur@users.noreply.github.com>
2023-01-04 13:23:20 -0800

    updated test

---
+++ tests/SynthDist_test.py
@@ -0,0 +1,21 @@
+import SynthDist
+import unittest
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+class Test_SynthDist(unittest.TestCase):
+
+    def __init__(self):
+        pass
+
+    def test_read_inputs(self):
+        pass
+
+
+
+
+
+# if __name__ == '__main__':
+#     unittest.main()
