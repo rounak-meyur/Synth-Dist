7e20fe82e6b5338fd8f714f0eadc762f6a80a0bc
rounak-meyur <rm5nz@virginia.edu>
2024-10-18 14:38:06 -0700

    updated main script

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 
 def main():
+    import os
     import argparse
     from omegaconf import OmegaConf
     parser = argparse.ArgumentParser()
@@ -22,14 +23,32 @@ def main():
 
     # Load inputs
     from utils.dataloader import load_homes
-    from utils.osm_utils import load_roads, map_homes_to_edges
+    from utils.osm_utils import load_roads
 
     input_home_csv = conf["inputs"]["home_csv"]
     homes = load_homes(file_path=input_home_csv)
     roads = load_roads(homes)
-    h2r = map_homes_to_edges(roads, homes, padding_distance=conf["mapping"]["padding"])
-    logger.info(h2r)
 
+    # Map roads and homes
+    from utils.mapping import(
+        map_homes_to_edges, 
+        write_mapping_to_file, 
+        read_mapping_from_file, 
+        compute_edge_to_homes_map
+    )
+
+    h2r_map_file = conf["outputs"]["home_to_road"]
+    if not os.path.exists(h2r_map_file):
+        h2r = map_homes_to_edges(
+            roads, homes, 
+            padding_distance=conf["mapping"]["padding"])
+        write_mapping_to_file(h2r, h2r_map_file)
+    else:
+        h2r = read_mapping_from_file(homes, filename=h2r_map_file)
+
+    r2h = compute_edge_to_homes_map(h2r)
+
+    # Generate secondary network
 
 if __name__ == "__main__":
     main()
